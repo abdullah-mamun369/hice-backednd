4080d9af278a669863cf1f427f71e534c40acfbe
abdullah-mamun369 <maamamun369@gmail.com>
2025-07-10 23:57:42 +0600

    Admin CRUD completed

--- src/app/modules/Admin/admin.route.ts
+++ src/app/modules/Admin/admin.route.ts
@@ -1,35 +1,35 @@
-// import express from "express";
-// import validateRequest from "../../middlewares/validateRequest";
-// import { AdminControllers } from "./admin.controller";
-// import { updateAdminValidationSchema } from "./admin.validation";
+import express from "express";
+import validateRequest from "../../middlewares/validateRequest";
+import { AdminControllers } from "./admin.controller";
+import { updateAdminValidationSchema } from "./admin.validation";
 // import { USER_ROLE } from "../user/user.constant";
 // import auth from "../../middlewares/auth";
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.get(
-//   "/",
-//   auth(USER_ROLE.superAdmin, USER_ROLE.admin),
-//   AdminControllers.getAllAdmins
-// );
+router.get(
+  "/",
+  //   auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  AdminControllers.getAllAdmins,
+);
 
-// router.get(
-//   "/:id",
-//   auth(USER_ROLE.superAdmin, USER_ROLE.admin),
-//   AdminControllers.getSingleAdmin
-// );
+router.get(
+  "/:id",
+  //   auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  AdminControllers.getSingleAdmin,
+);
 
-// router.patch(
-//   "/:id",
-//   auth(USER_ROLE.superAdmin),
-//   validateRequest(updateAdminValidationSchema),
-//   AdminControllers.updateAdmin
-// );
+router.patch(
+  "/:id",
+  //   auth(USER_ROLE.superAdmin),
+  validateRequest(updateAdminValidationSchema),
+  AdminControllers.updateAdmin,
+);
 
-// router.delete(
-//   "/:adminId",
-//   auth(USER_ROLE.superAdmin),
-//   AdminControllers.deleteAdmin
-// );
+router.delete(
+  "/:id",
+  //   auth(USER_ROLE.superAdmin),
+  AdminControllers.deleteAdmin,
+);
 
-// export const AdminRoutes = router;
+export const AdminRoutes = router;
